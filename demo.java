30c5999d7532c4852bff81c1f2b88ca837fa0b3d
Abhinandanm2000 <92453036+Abhinandanm2000@users.noreply.github.com>
2021-10-14 16:50:01 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,51 @@
+
+public class demo {
+
+	public static void main(String[] args) {
+
+	 
+	//arithmetic
+	 int a=45;
+	 int b=15;
+	 System.out.println(a+b);
+	 System.out.println(a-b);
+	 System.out.println(a*b);
+	 System.out.println(a/b);
+	 
+	 //logical
+
+	 System.out.println((5<3)&&(8>5));
+	 System.out.println((5<4)||(8>5));
+	 System.out.println(!(5==0));
+	 
+	 //assignment
+	 int c=3;
+     int d=c;
+     System.out.println(d);
+     d+=3;
+     System.out.println(d);
+     d-=3;
+     System.out.println(d);
+     d*=3;
+     System.out.println(d);
+     d/=3;
+     System.out.println(d);
+     
+     
+     
+     
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+
+	}
+
+}
+
+
+
