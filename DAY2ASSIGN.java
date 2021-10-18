8e15b6bfa6148a62f7f9c5e1216416028540b119
Abhinandanm2000 <92453036+Abhinandanm2000@users.noreply.github.com>
2021-10-18 15:43:04 +0530

    Add files via upload

---
+++ DAY2ASSIGN.java
@@ -0,0 +1,138 @@
+
+public class DAY2ASSIGN {
+
+	public static void main(String[] args) {
+	 
+		//elements of an arry
+		//int arr[]= {1,2,3,3,4};
+		
+	/*  for(int i=0;i<arr.length;i++) {
+		  System.out.println(arr[i]);
+	 
+	  } */
+	 
+		
+		
+		
+		//elements in reverse order
+		
+		/* for(int i=arr.length-1;i>=0;i--) {
+		  System.out.println(arr[i]);
+		 
+		  
+	  } */
+		
+		
+		
+		//to print duplicate elements
+	
+		/* System.out.println("Duplicate element: ");  
+	        
+	        for(int i = 0; i < arr.length; i++) {  
+	            for(int j = i + 1; j < arr.length; j++) {  
+	                if(arr[i] == arr[j])  
+	                    System.out.println(arr[j]);  
+	            }
+	            
+	        } */
+		
+		
+		
+		
+		
+		//to sort the array
+	/*	int [] arr = {10,45,23,12,43,33,67,82};     
+	        int temp = 0;    
+	 
+	         
+	       System.out.println("The given array: ");    
+	       for (int i = 0; i <arr.length; i++) {     
+	           System.out.print(arr[i] + " ");    
+	        }    
+	         
+	        for (int i = 0; i <arr.length; i++) {     
+	          for (int j = i+1; j <arr.length; j++) {     
+	              if(arr[i] >arr[j]) {      
+	                 temp = arr[i];    
+	               arr[i] = arr[j];    
+	               arr[j] = temp;    
+	               }     
+	            }     
+	        }    
+	            
+	        System.out.println("\nThe sorted array: ");    
+	        for (int i = 0; i <arr.length; i++) {     
+	            System.out.print(arr[i] + " ");    
+	        }    */
+		
+	 
+		
+		 
+		
+	
+	
+	   
+	
+	//prime no
+
+	
+	/* int num =10;
+	    boolean flag=false;
+	    for (int i = 2; i <= num / 2; ++i) {
+	     
+	      if (num % i == 0) {
+	        flag = true;
+	        break;
+	      }
+	    }
+
+	    if (!flag)
+	      System.out.println(num + " is a prime number.");
+	    else
+	      System.out.println(num + " is not a prime number.");
+
+   */
+		
+		
+		
+		
+		// reversing the numbers
+		
+		/* int a[]={6,5,4,3,2,1};       
+        
+		System.out.println("numbers in revrese format:"); 
+        for(int i=a.length-1;i>=0;i--)
+        {
+            
+        	System.out.print(a[i] +" ");  
+        }   */
+		
+		
+		
+		//characters in string
+	   /*  String str = "Hello world";    
+        int count = 0;    
+        System.out.println("The string is: "+str);    
+           
+        for(int i = 0; i < str.length(); i++) 
+        {    
+            if(str.charAt(i) != ' ')    
+                count++;    
+        }                
+          
+        System.out.println("no characters in the string: " + count); 
+		
+		
+        String str1 = "Hello world";
+        System.out.println(str1.length());  */
+		
+		  
+	
+			
+     
+        
+	
+
+}
+
+}
